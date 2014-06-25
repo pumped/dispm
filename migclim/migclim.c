06d376dba11a28b9070bbd39889a3aed22b2d7ee
Dylan M <pumpedpwned@gmail.com>
2014-06-26 00:22:35 +1000

    Migclim now requires param, input, output paths (not fully implemented)

--- migclim/migclim.c
+++ migclim/migclim.c
@@ -7,14 +7,25 @@
 #include "univ_disp.c"
 #include "src_cell.c"
 
-
-
+// arguments params file, input, output
 int main( int argc, char const *argv[] ) {
-	/* code */
-	char *param = "mig_mic_curr/params.txt";
-	int nrFiles = 12;
+	if (argc == 4) {
+		
+		char const *param = argv[1];
+		char const *inputDirectory = argv[2];
+		char const *outputDirectory = argv[3];
+		int nrFiles = 12;
 
-	mcMigrate(&param, &nrFiles);
+		printf("Using %s\n", param);
 
-	return 0;
+		mcMigrate(&param, &nrFiles);
+
+		return 0;
+
+	} else {
+		
+		printf("\n\nInvalid arguments. \nExpected:\nmig [parameter file location] [input location] [output location]\n\n");
+		return -1;
+		
+	}	
 }
